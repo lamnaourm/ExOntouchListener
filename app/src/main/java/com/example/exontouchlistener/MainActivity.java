90fae528598c27a504c3382c24bb5571974572a5
bob <mohammed.lamnaour@gmail.com>
2022-04-26 13:02:14 +0000

    Version 1 - afficher les coordonnees

--- app/src/main/java/com/example/exontouchlistener/MainActivity.java
+++ app/src/main/java/com/example/exontouchlistener/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.exontouchlistener;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView im;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        im = findViewById(R.id.img);
+
+        im.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+
+                Toast.makeText(MainActivity.this, "X = " + motionEvent.getX() + " - " + motionEvent.getY(), Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
     }
 }
